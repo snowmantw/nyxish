c6ba1395d5f5f045afc5e4e96e0f46b39cbb6fba
Greg Weng <snowmantw@gmail.com>
2014-05-09 10:06:40 +0800

    WIP - try to implement runtime

--- library/Runtime.js
+++ library/Runtime.js
@@ -1,4 +1,4 @@
-define('Runtime', ['State', 'Transfer', 'TransferInterfaces'],
+define('Runtime', ['State', 'Transfer', 'TransferInterfaces', 'modules/DefaultStates'],
 function(State, Transfer, Monitor) {
   var Runtime = function() {
     if (window.__runtime__) {
@@ -6,7 +6,50 @@ function(State, Transfer, Monitor) {
     }
 
     window.__runtime__ = this;
+    this.setup();
     return this;
+  };
+
+  Runtime.prototype.setup = function() {
+    this.states = {};
+    this.states.module = {
+      'active': null,
+      'modules': {}
+    };
+  };
+
+  Runtime.prototype.start = function() {
+    this.registerModule(new DefaultStates());
+  };
+
+  Runtime.prototype.stop = function() {
+    this.states.module.modules.forEach((mod) => {
+      this.unregisterModule(mod);
+    });
+  };
+
+  Runtime.registerModule = function(mod) {
+    this.states.module.modules[mod.name] = mod;
+  };
+
+  Runtime.unregisterModule = function(mod) {
+    delete this.states.module.modules[mod.name];
+  };
+
+  Runtime.prototype.transfer = function(generator, pred) {
+    if ('function' !== typeof generator) {
+      return this.transferInterfaces();
+    }
+
+    var transfer = new Transfer(this,
+        this.states.module.active.context(),
+        generator,
+        pred);
+    transfer.execute();
+  };
+
+  Runtime.prototype.transferInterfaces = function() {
+    return new TransferInterfaces(this, this.states.module.active.context());
   };
 
   return Runtime;
